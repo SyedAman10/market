9f8db1e077fd9fd84c050265f1352cd732f45faa
Syed Aman <amanullahnaqvi@gmail.com>
2022-05-06 17:04:03 +0500

    completed login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,224 @@
+import 'dart:convert';
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login_ui/common/theme_helper.dart';
+
+import 'forgot_password_page.dart';
+import 'profile_page.dart';
+import 'registration_page.dart';
+import 'widgets/header_widget.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  double _headerHeight = 250;
+  Key _formKey = GlobalKey<FormState>();
+
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  Future<bool> login(username, password) async {
+    // if (username.text == "" || password.text == "") {
+    //   Fluttertoast.showToast(
+    //     msg: "Both fields cannot be blank",
+    //     toastLength: Toast.LENGTH_SHORT,
+    //     gravity: ToastGravity.CENTER,
+    //   );
+    //   return null;
+    // } else {
+    //   var res = await http.post(Uri.parse(url), body: {
+    //     "username": username.text,
+    //     "password": password.text,
+    //   });
+
+    //   var data = json.decode(res.body);
+    //   if (data == 'success') {
+    //     Navigator.pushReplacement(
+    //         cont, MaterialPageRoute(builder: (context) => ProfilePage()));
+    //   } else {
+    //     Fluttertoast.showToast(
+    //       msg: "Account does not exists",
+    //       toastLength: Toast.LENGTH_SHORT,
+    //       gravity: ToastGravity.CENTER,
+    //     );
+    //   }
+    // }
+
+    var url = "http://localhost/localconnection/connection.php";
+
+    var res = await http.post(Uri.parse(url), body: {
+      "username": username,
+      "password": password,
+    });
+
+    var data = json.decode(res.body);
+    if (data == 'success') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: _headerHeight,
+              child: HeaderWidget(_headerHeight, true,
+                  Icons.login_rounded), //let's create a common header widget
+            ),
+            SafeArea(
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  margin: EdgeInsets.fromLTRB(
+                      20, 10, 20, 10), // This will be the login form
+                  child: Column(
+                    children: [
+                      Text(
+                        'Hello',
+                        style: TextStyle(
+                            fontSize: 60, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Sign in into your account',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(height: 30.0),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Container(
+                                child: TextField(
+                                  controller: username,
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      'User Name', 'Enter your user name'),
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              ),
+                              SizedBox(height: 30.0),
+                              Container(
+                                child: TextField(
+                                  controller: password,
+                                  obscureText: true,
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      'Password', 'Enter your password'),
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              ),
+                              SizedBox(height: 15.0),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                alignment: Alignment.topRight,
+                                child: GestureDetector(
+                                  onTap: () async {
+                                    if (username.value.toString() == "" ||
+                                        password.value.toString() == "") {
+                                      Fluttertoast.showToast(
+                                        msg: "Both fields cannot be blank",
+                                        toastLength: Toast.LENGTH_SHORT,
+                                        gravity: ToastGravity.CENTER,
+                                      );
+                                      var isSuccessful = false;
+                                    } else {
+                                      var isSuccessful = await login(
+                                          username.text, password.text);
+                                      if (isSuccessful) {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    ProfilePage()));
+                                      } else {
+                                        Fluttertoast.showToast(
+                                          msg: "An error occured.",
+                                          toastLength: Toast.LENGTH_SHORT,
+                                          gravity: ToastGravity.CENTER,
+                                        );
+                                      }
+                                    }
+                                  },
+                                  child: Text(
+                                    "Forgot your password?",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                decoration:
+                                    ThemeHelper().buttonBoxDecoration(context),
+                                child: ElevatedButton(
+                                  style: ThemeHelper().buttonStyle(),
+                                  child: Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                    child: Text(
+                                      'Sign In'.toUpperCase(),
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    //After successful login we will redirect to profile page. Let's create profile page now
+                                    // login(context);
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ProfilePage()));
+                                  },
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                //child: Text('Don\'t have an account? Create'),
+                                child: Text.rich(TextSpan(children: [
+                                  TextSpan(text: "Don\'t have an account? "),
+                                  TextSpan(
+                                    text: 'Create',
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    RegistrationPage()));
+                                      },
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context).accentColor),
+                                  ),
+                                ])),
+                              ),
+                            ],
+                          )),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
